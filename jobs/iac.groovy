6ec8df4891c999e7ba5a9dd036fa74f58c970bea
Ganga1993 <gangadharrao.pasupuleti@gmail.com>
2018-03-14 15:03:42 +0530

    Password job

--- jobs/iac.groovy
+++ jobs/iac.groovy
@@ -8,6 +8,12 @@ pipelineJob('test-pg-job') {
   wrappers {
     maskPasswords()
   }
+  configure {
+    it / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' << 'hudson.model.PasswordParameterDefinition' {
+            name('master_password')
+            description('password for rds database')
+        }
+  }
   definition {
     cps {
         script(readFileFromWorkspace('pipeline/test.groovy'))
